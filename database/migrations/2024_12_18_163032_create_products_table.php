51ec0066c99817aca56d43fe3d089027de42d59e
kavindu919 <jayakodyk2@gmail.com>
2024-12-19 22:26:19 +0530

    Complete the migrations

--- database/migrations/2024_12_18_163032_create_products_table.php
+++ database/migrations/2024_12_18_163032_create_products_table.php
@@ -19,6 +19,11 @@ return new class extends Migration
             $table->string('slug')->unique();
             $table->json('images')->nullable();
             $table->longText('description')->nullable();
+            $table->decimal('price',10, 2);
+            $table->boolean('is_active')->default(true);
+            $table->boolean('is_featured')->default(false);
+            $table->boolean('in_stock')->default(true);
+            $table->boolean('on_sale')->default(false);
             $table->timestamps();
         });
     }
