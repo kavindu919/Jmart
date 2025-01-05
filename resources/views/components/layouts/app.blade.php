0252d29393c27f5e5d764fa26a773dce02409ae4
kavindu919 <jayakodyk2@gmail.com>
2025-01-05 18:32:30 +0530

    Start to implemt the fronted side

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>{{ $title ?? 'JMart' }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @livewireStyles()
+</head>
+
+<body>
+    <main class="bg-slate-200 dark:bg-slate-700">
+        {{ $slot }}
+    </main>
+    @livewireScripts()
+</body>
+
+</html>
