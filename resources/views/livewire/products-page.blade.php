2929ceeb4f3dbee8098f4cc1d4025d77a72ac753
kavindu919 <jayakodyk2@gmail.com>
2025-01-12 16:03:52 +0530

    Complete Filter Option on product view

--- resources/views/livewire/products-page.blade.php
+++ resources/views/livewire/products-page.blade.php
@@ -11,7 +11,7 @@
                                 <li class="mb-4" wire:key="{{ $category->id }}">
                                     <label for="{{ $category->slug }}" class="flex items-center dark:text-gray-400 ">
                                         <input id="{{ $category->slug }}" value="{{ $category->id }}" type="checkbox"
-                                            class="w-4 h-4 mr-2">
+                                            wire:model.live="selected_categories" class="w-4 h-4 mr-2">
                                         <span class="text-lg">{{ $category->name }}</span>
                                     </label>
                                 </li>
@@ -29,8 +29,8 @@
                             @foreach ($brands as $brand)
                                 <li class="mb-4" wire:key="{{ $brand->id }}">
                                     <label for="{{ $brand->slug }}" class="flex items-center dark:text-gray-300">
-                                        <input type="checkbox" id="{{ $brand->slug }}" value="{{ $brand->id }}"
-                                            class="w-4 h-4 mr-2">
+                                        <input wire:model.live="selected_brands" type="checkbox"
+                                            id="{{ $brand->slug }}" value="{{ $brand->id }}" class="w-4 h-4 mr-2">
                                         <span class="text-lg dark:text-gray-400">{{ $brand->name }}</span>
                                     </label>
                                 </li>
@@ -44,14 +44,16 @@
                         <div class="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400"></div>
                         <ul>
                             <li class="mb-4">
-                                <label for="" class="flex items-center dark:text-gray-300">
-                                    <input type="checkbox" class="w-4 h-4 mr-2">
-                                    <span class="text-lg dark:text-gray-400">In Stock</span>
+                                <label for="featured" class="flex items-center dark:text-gray-300">
+                                    <input id="featured" wire:model.live="featured" value="1" type="checkbox"
+                                        class="w-4 h-4 mr-2">
+                                    <span class="text-lg dark:text-gray-400">Featured Products</span>
                                 </label>
                             </li>
                             <li class="mb-4">
-                                <label for="" class="flex items-center dark:text-gray-300">
-                                    <input type="checkbox" class="w-4 h-4 mr-2">
+                                <label for="on_sale" class="flex items-center dark:text-gray-300">
+                                    <input id="on_sale" type="checkbox" class="w-4 h-4 mr-2" value="1"
+                                        wire:model.live="on_sale">
                                     <span class="text-lg dark:text-gray-400">On Sale</span>
                                 </label>
                             </li>
@@ -62,12 +64,15 @@
                         <h2 class="text-2xl font-bold dark:text-gray-400">Price</h2>
                         <div class="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400"></div>
                         <div>
-                            <input type="range"
+                            <div class="font-semibold text-blue-400">{{ Number::currency($price_range, 'LKR') }}</div>
+                            <input type="range" wire:model.live='price_range'
                                 class="w-full h-1 mb-4 bg-blue-100 rounded appearance-none cursor-pointer"
                                 max="500000" value="100000" step="100000">
                             <div class="flex justify-between ">
-                                <span class="inline-block text-lg font-bold text-blue-400 ">&#8377; 1000</span>
-                                <span class="inline-block text-lg font-bold text-blue-400 ">&#8377; 500000</span>
+                                <span
+                                    class="inline-block text-lg font-bold text-blue-400 ">{{ Number::currency(1000, 'LKR') }}</span>
+                                <span
+                                    class="inline-block text-lg font-bold text-blue-400 ">{{ Number::currency(500000, 'LKR') }}</span>
                             </div>
                         </div>
                     </div>
