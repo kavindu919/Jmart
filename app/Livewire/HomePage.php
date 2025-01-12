b458f8dc4200cc14e7253ecfcc96c29f77bc62fe
kavindu919 <jayakodyk2@gmail.com>
2025-01-12 07:52:53 +0530

    Implement product and product details page

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
 use Livewire\Component;
+
 
 class HomePage extends Component
 {
     public function render()
     {
-        return view('livewire.home-page');
+        $brands = Brand::where('is_active', 1)->get();
+        $categoris = Category::where('is_active', 1)->get();
+        return view('livewire.home-page', ['brands' => $brands, 'categoris' => $categoris]);
     }
 }
