8b1f98849a26fcbe7ef7daa9b060d191d918705e
kavindu919 <jayakodyk2@gmail.com>
2025-01-22 21:40:59 +0530

    Integrate with stripe and start to checkout page

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -3,6 +3,9 @@
 namespace App\Livewire;
 
 use App\Helpers\CartManagement;
+use App\Models\Address;
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class CheckoutPage extends Component
@@ -28,6 +31,44 @@ class CheckoutPage extends Component
             'zip_code' => 'required',
             'payment_method' => 'required',
         ]);
+        $cart_items = CartManagement::getCartItemsFromCookie();
+        $line_items = [];
+        foreach ($cart_items as $item) {
+            $line_items[] = [
+                'price' => [
+                    'currency' => 'lkr',
+                    'unit_amount' => $item['unit_amount'] * 300,
+                    'product_data' => [
+                        'name' => $item['name']
+                    ],
+                    'quantity' => $item['quantity']
+                ]
+            ];
+        }
+        $order = new Order();
+        $order->user_id = Auth::id();
+        $order->grand_total = CartManagement::calculateGrandTotal($cart_items);
+        $order->payment_method = $this->payment_method;
+        $order->payment_status = 'pending';
+        $order->status = 'new';
+        $order->currency = 'lkr';
+        $order->shipping_amount = 0;
+        $order->shipping_method = 'none';
+        $order->notes = 'Order placed by' . Auth::user()->name;
+
+        $address = new Address();
+        $address->first_name = $this->first_name;
+        $address->last_name = $this->last_name;
+        $address->phone = $this->phone;
+        $address->street_address = $this->street_address;
+        $address->city = $this->city;
+        $address->state = $this->state;
+        $address->zip_code = $this->zip_code;
+
+        $redirect_url = '';
+        if ($this->payment_method == 'stripe') {
+        } else {
+        }
     }
 
     public function render()
