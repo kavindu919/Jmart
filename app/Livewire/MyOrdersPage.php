d5fb713c9e0bb24413f45d5d192697177cc413c1
kavindu919 <jayakodyk2@gmail.com>
2025-01-30 23:26:07 +0530

    Complete the my orders page

--- app/Livewire/MyOrdersPage.php
+++ app/Livewire/MyOrdersPage.php
@@ -2,12 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class MyOrdersPage extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.my-orders-page');
+        $my_orders = Order::where('user_id', Auth::id())->latest()->paginate(2);
+        return view('livewire.my-orders-page', [
+            'orders' => $my_orders
+        ]);
     }
 }
